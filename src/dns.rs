d58aa56a8d9984906ca443d9d37c9edcf53a7d61
Maciej Krüger <mkg20001@gmail.com>
2026-03-06 15:19:54 +0100

    fix: def address dns

--- src/dns.rs
+++ src/dns.rs
@@ -344,7 +344,7 @@ impl DnsServer {
     ) -> Result<()> {
         let addr: SocketAddr = format!(
             "{}:{}",
-            self.config.address.as_deref().unwrap_or("::"),
+            self.config.address.as_deref().unwrap_or("[::]"),
             self.config.port
         )
         .parse()?;
